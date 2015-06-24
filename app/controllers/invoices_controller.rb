085bf2ac2619dd9566c52fbf7d3bd556d254872b
Maciej Stanisz <mac.stanisz@gmail.com>
2015-06-24 14:45:25 +0200

    invoicing basics

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,5 +1,7 @@
 class InvoicesController < ApplicationController
   def index
+    @buyer_first_name = 'Andrzej'
+    @buyer_last_name = 'Jakcinaimie'
     respond_to do |format|
       format.html
       format.pdf do
