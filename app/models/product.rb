75f8c364dc95745f06eba24e572fa9f79519859b
Ciabas <ciabas12@interia.pl>
2015-06-24 16:37:45 +0200

    [fix]some issue with starting server

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
   belongs_to :gear
   belongs_to :category
   has_many :product_images
-  accepts_nested_attributes_for :product_images, :reject_if => lambda { |t| t['trip_image'].nil? }
+  accepts_nested_attributes_for :product_images
 
   validates :name, :price, presence: true 
   validates :price, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => {:greater_than => 0}
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw destroy
+Rails.application.routes.draw do
   devise_for :users
   root 'home#home'
   resources :profiles, except:  [:index, :destroy]
