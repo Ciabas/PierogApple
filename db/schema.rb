cdeb77d091e8693f56065dfbf974681909ed443c
Daria Woźnicka <woznicka.daria@gmail.com>
2015-06-25 11:15:09 +0200

    [fix] added telephone number to users profile

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150622142827) do
   create_table "profiles", force: :cascade do |t|
     t.string   "first_name",   null: false
     t.string   "last_name",    null: false
+    t.string   "phone_no",     null: false
     t.string   "street_name",  null: false
     t.string   "house_no",     null: false
     t.integer  "apartment_no", null: false
