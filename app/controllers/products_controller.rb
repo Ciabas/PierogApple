b872c99bac1561675c425bdae160bd8e1806af56
Daria Woźnicka <woznicka.daria@gmail.com>
2015-07-02 14:30:53 +0200

    [modify] similar products visible and filters fixed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,37 +8,10 @@ class ProductsController < ApplicationController
   
   def show
     @product = Product.find(params[:id])
-  end
-
-  def new
-    @product = Product.new
-    @categories = Category.all 
-    @gears = Gear.all
-    3.times {@product.product_images.build}
-  end
-
-  def create
-    @product = Product.new(product_params)
-    @categories = Category.all 
-    @gears = Gear.all
-    if @product.save
-      redirect_to root_url, notice: 'Produkt dodany'
-    else 
-      render :new, error: 'Błąd, sprawdź parametry'
-    end
-  end
-
-  def destroy
-    @product = Product.find(params[:id])
-    @product.destroy
-    redirect_to root_url
+    @similar = Product.where(category_id: @product.category_id, gear_id: @product.gear_id)
   end
 
   private
-  
-  def product_params
-    params.require(:product).permit(:name, :price, :category_id, :gear_id, :description, product_images_attributes: [:caption, :photo])
-  end
   
   def params_check    
     products = Product.all
