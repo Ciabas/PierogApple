42289cc2097b2161ac4723e76811a1e4fbffed33
Daria Woźnicka <woznicka.daria@gmail.com>
2015-07-02 11:33:04 +0200

    [fix] fixed types filters

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -2,11 +2,8 @@ class Admin::ProductsController < Admin::AdminController
   
   def index
     @categories = Category.all
-    if params[:category_id].blank?
-      @products = Product.all
-    else
-      @products = Product.where(category_id: params[:category_id])
-    end
+    @gears = Gear.base
+    @products, @cid, @price_low, @price_high, @models, @g, @m = params_check
   end
 
   def show
@@ -60,7 +57,35 @@ class Admin::ProductsController < Admin::AdminController
   end
 
   private
+
   def product_params
     params.require(:product).permit(:name, :price, :category_id, :gear_id, :description, product_images_attributes: [:caption, :photo])
+  end
+
+  def params_check    
+    products = Product.all
+    unless params[:cid].blank?
+      products = products.where( category_id: params[:cid] )
+      cid = params[:cid]
+    end
+    unless params[:price_min].blank?
+      products = products.where( [ 'price >= ?', params[:price_min] ] )
+      price_low = params[:price_min]
+    end
+    unless params[:price_max].blank?
+      products = products.where( [ 'price <= ?', params[:price_max] ] )
+      price_high = params[:price_max]
+    end
+    unless params[:g].blank?
+      gear = Gear.where(parent_id: params[:g])
+      products = products.where( gear_id: gear )
+      models = Gear.where( parent_id: params[:g] )
+      g = params[:g]
+    end
+    unless params[:m].blank?
+      products = products.where( gear_id: params[:m] )
+      m = params[:m]
+    end
+    return products, cid, price_low, price_high, models, g, m  
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150701132540) do
+ActiveRecord::Schema.define(version: 20150624122052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,15 +60,6 @@ ActiveRecord::Schema.define(version: 20150701132540) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "slider_images", force: :cascade do |t|
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.string   "photo_file_name"
-    t.string   "photo_content_type"
-    t.integer  "photo_file_size"
-    t.datetime "photo_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
