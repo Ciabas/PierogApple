90f329d1312a9f5c69f177d7610857341493ddb5
Maciej Stanisz <mac.stanisz@gmail.com>
2015-07-03 11:25:52 +0200

    [add] prepared to integrate with orders

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -2,35 +2,20 @@ class InvoicesController < ApplicationController
   
   before_action :check_token
 
-  def index
-    
-    now = Time.new
-    
-    @invoice_number = '1'
-    @current_year = now.year.to_s
-    
-    @transaction_date = now.strftime("%Y-%m-%d")
-    @invoicing_date = now.strftime("%Y-%m-%d")
-    
-    #static data kept in a hash
-    @buyer = {email: "foo@bar.com"}
-    @buyer_profile = {first_name: "Andrzej", last_name: "Jakcinaimie", street_name: "Uliczna", house_no: "15", apartment_no: 6, zip_code: "90-210", city_name: "Łódź", phone_no: "500500500"}
-    
-    @sum = 304.5
-    
+  def invoice
+    @order=Order.find(params[:id])
     
     respond_to do |format|
       format.html
       format.pdf do
-        render pdf: "faktura"   # Excluding ".pdf" extension.
+        render pdf: "ApplePierog - faktura nr #{@order.id}"   # Excluding ".pdf" extension.
       end
     end
   end
 
   private
   def check_token
-    #unless params[:token]==Invoices.find[:id].token #that's how it should be done
-    unless params[:token]=="aaa"
+    unless params[:token]==Order.find[:id].token
       redirect_to root_url
     end
   end
