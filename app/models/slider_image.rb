0766f9ddad79c25f884bcdac9435d461d8c35ccc
Maciej Stanisz <mac.stanisz@gmail.com>
2015-07-06 13:23:55 +0200

    [add] paperclip error messages localization

--- app/models/slider_image.rb
+++ app/models/slider_image.rb
@@ -1,6 +1,6 @@
 class SliderImage < ActiveRecord::Base
   has_attached_file :photo, :styles => { :slider => "1400x600#", :miniature => "50x50#" }
-  validates_attachment_presence :photo, :message => 'Nie dodano zdjęcia'
-  validates_attachment_size :photo, :less_than => 5.megabytes, :message => 'Za duży rozmiar pliku'
-  validates_attachment_content_type :photo, :content_type => %w(image/jpeg image/jpg image/png), :message => 'Niewłaściwy format zdjęcia'
+  validates_attachment_presence :photo
+  validates_attachment_size :photo, :less_than => 5.megabytes
+  validates_attachment_content_type :photo, :content_type => %w(image/jpeg image/jpg image/png)
 end
