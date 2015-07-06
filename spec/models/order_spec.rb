67e5454bbdc4fd18de224f33827dc78310f67d26
wojtekparol <wojtekparol31@gmail.com>
2015-07-06 14:39:53 +0200

    [add] spec for order

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Order, :type => :model do
+  it { should have_many(:order_products) }
+  it { should have_many(:products).through(:order_products) }
+  it { should validate_presence_of(:company_first_name) }
+  it { should validate_presence_of(:company_first_name) }
+  it { should validate_presence_of(:company_street_name) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:name) }
 end
