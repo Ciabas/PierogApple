a53012092c6137adbedb7e27dba13e7f25bd642e
Maciej Stanisz <mac.stanisz@gmail.com>
2015-07-09 12:18:26 +0200

    [fix] put faker operations in blocks

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    email Faker::Internet.email
-    password Faker::Internet.password
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
 
     trait :admin do
       is_admin true
@@ -13,7 +13,7 @@ FactoryGirl.define do
   end
 
   factory :gear do
-    name Faker::Commerce.department
+    name { Faker::Commerce.department }
 
     factory :model do
       @gear = FactoryGirl.create(:gear)
@@ -22,33 +22,33 @@ FactoryGirl.define do
   end
 
   factory :category do
-    name Faker::Commerce.department
+    name { Faker::Commerce.department }
   end
 
   factory :profile do
     # untested - we'll see soon
     user
-    first_name Faker::Name.first_name
-    last_name Faker::Name.last_name
-    phone_no Faker::Number.number(9)
-    street_name Faker::Address.street_name
-    house_no Faker::Address.building_number
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    phone_no { Faker::Number.number(9) }
+    street_name { Faker::Address.street_name }
+    house_no { Faker::Address.building_number }
     zip_code '90-210'
-    city_name Faker::Address.city
-    apartment_no Faker::Number.number(2)
+    city_name { Faker::Address.city }
+    apartment_no { Faker::Number.number(2) }
   end
 
   factory :product_image do
-    photo Faker::Avatar.image
+    photo { Faker::Avatar.image }
     product
   end
 
   factory :product do
     category
     association :gear, factory: :model
-    name Faker::Commerce.department
-    price Faker::Commerce.price
-    description Faker::Lorem.paragraph
+    name { Faker::Commerce.department }
+    price { Faker::Commerce.price }
+    description { Faker::Lorem.paragraph }
     status 'dostepny'
 
     transient do
